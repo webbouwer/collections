fac5a54c10108721a777cfbf55aa63a21574b105
Webbouwer <support@webdesigndenhaag.net>
2021-08-16 17:53:52 +0200

    Added collections and type taxonomies and artifact posttype

--- collections.php
+++ collections.php
@@ -1,9 +1,18 @@
 <?php
 /*
 Plugin Name: Collections Plugin
-Plugin URI: https://webbouwer.org
-Description: Skeleton code for Wordpress plugin
+Plugin URI: https://github.com/webbouwer
+Description: Collections plugin for Wordpress
+
+Suggestion to use with
+https://wordpress.org/plugins/taxonomy-terms-order
+https://wordpress.org/plugins/admin-taxonomy-filter/
 */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+require_once(plugin_dir_path(__FILE__) . 'func/taxonomy_types.php');
+require_once(plugin_dir_path(__FILE__) . 'func/taxonomy_collection.php');
+require_once(plugin_dir_path(__FILE__) . 'func/posttype_artifact.php');
