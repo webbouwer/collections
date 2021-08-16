d822e829a1dac6252bcfb660e4b9bb122b60b9b4
Webbouwer <support@webdesigndenhaag.net>
2021-08-16 21:41:15 +0200

    Check & rename files

--- collections.php
+++ collections.php
@@ -13,6 +13,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require_once(plugin_dir_path(__FILE__) . 'func/taxonomy_types.php');
-require_once(plugin_dir_path(__FILE__) . 'func/taxonomy_collection.php');
-require_once(plugin_dir_path(__FILE__) . 'func/posttype_artifact.php');
+require_once(plugin_dir_path(__FILE__) . 'function/taxonomy_types.php');
+require_once(plugin_dir_path(__FILE__) . 'function/taxonomy_collection.php');
+require_once(plugin_dir_path(__FILE__) . 'function/posttype_artifact.php');
+
+
+/*
+add_shortcode( 'collection', 'display_collection');
+
+function display_collection( $atts = array() ) {
+
+    // set up default parameters
+    extract(shortcode_atts(array(
+     'id' => 0, // 0 = all, or specific id
+		 'view' => 'full', // full = full list including filter and display menu's, simple = only a list
+    ), $atts));
+
+    return "<img src=\"http://dayoftheindie.com/wp-content/uploads/$rating-star.png\"
+    alt=\"doti-rating\" width=\"130\" height=\"188\" class=\"left-align\" />";
+}
+*/
