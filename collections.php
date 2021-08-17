23577d341046a0c28cd4685e4d6e6d7c40f76b1c
Webbouwer <support@webdesigndenhaag.net>
2021-08-17 08:32:01 +0200

    Add standard header and footer code

--- collections.php
+++ collections.php
@@ -18,7 +18,30 @@ require_once(plugin_dir_path(__FILE__) . 'function/taxonomy_collection.php');
 require_once(plugin_dir_path(__FILE__) . 'function/posttype_artifact.php');
 
 
-/*
+
+add_filter('template_include', 'taxonomy_template');
+function taxonomy_template( $template ){
+
+	if( is_tax('collection')){
+	    $template = dirname( __FILE__ ) .'/template/taxonomy-collection.php';
+	}
+	return $template;
+
+}
+
+// https://stackoverflow.com/questions/19328475/adding-custom-page-template-from-plugin
+add_filter( 'single_template', 'single_post_template' ) ;
+function single_post_template($single_template) {
+
+	global $post;
+	if ( $post->post_type == 'artifact' ) {
+		$single_template = dirname( __FILE__ ) . '/template/posttype-artifact.php';
+	}
+	return $single_template;
+	
+}
+
+/* https://pagely.com/blog/creating-custom-shortcodes/
 add_shortcode( 'collection', 'display_collection');
 
 function display_collection( $atts = array() ) {
